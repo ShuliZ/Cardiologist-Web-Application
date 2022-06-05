a3e4acc40536600fcd83be1d098d11d4d06a217f
Shuli Zhu <shulizhu2022@u.northwestern.edu>
2022-06-05 02:16:09 -0500

    Updated pipeline

--- run.py
+++ run.py
@@ -1,17 +1,19 @@
 """Configures the subparsers for receiving command line arguments for each
  stage in the model pipeline and orchestrates their execution."""
 import os
+import sys
+import pickle
 import argparse
 import logging.config
 
 import yaml
-import joblib
 import pandas as pd
 
 from config.flaskconfig import SQLALCHEMY_DATABASE_URI
 from src.add_patients import create_db, PatientManager
-from src.feature import get_binary_data, get_ohe_data, get_ordinalenc_age, get_ordinalenc_health
-from src.model import train, evaluate
+from src.acquire import import_data
+from src.feature import featurize
+from src.model import train, score, evaluate, plot_feature_importance
 from src.s3 import upload_file_to_s3, download_file_from_s3
 
 logging.config.fileConfig("config/logging/local.conf")
@@ -53,42 +55,27 @@ if __name__ == "__main__":
                            help="Local path to download files to")
 
 
-    # Sub-parser for ingesting new data
-    sb_ingest = subparsers.add_parser("ingest", description="Add data to database")
-    sb_ingest.add_argument("--id", help="patient ID")
-    sb_ingest.add_argument("--bmi", help="")
-    sb_ingest.add_argument("--smoking", help="")
-    sb_ingest.add_argument("--alcohol_drinking", help="")
-    sb_ingest.add_argument("--stroke", help="")
-    sb_ingest.add_argument("--physical_health", help="")
-    sb_ingest.add_argument("--mental_health", help="")
-    sb_ingest.add_argument("--diff_walking", help="")
-    sb_ingest.add_argument("--sex", help="")
-    sb_ingest.add_argument("--age_category", help="")
-    sb_ingest.add_argument("--race", help="")
-    sb_ingest.add_argument("--diabetic", help="")
-    sb_ingest.add_argument("--physical_activity", help="")
-    sb_ingest.add_argument("--gen_health", help="")
-    sb_ingest.add_argument("--sleep_time", help="")
-    sb_ingest.add_argument("--asthma", help="")
-    sb_ingest.add_argument("--kidney_disease", help="")
-    sb_ingest.add_argument("--skin_cancer", help="")
-    sb_ingest.add_argument("--engine_string", default='sqlite:///data/patient.db',
-                           help="SQLAlchemy Connection URI for database")
-
     # Sub-parser for acquiring, cleaning, and running model pipeline
     sb_pipeline = subparsers.add_parser("run_model_pipeline",
                                         description="Acquire data, clean data, "
                                                     "featurize data, and run model-pipeline")
     sb_pipeline.add_argument("--step", help="Which step to run",
-                             choices=["feature", "model", "test"])
-    sb_pipeline.add_argument("--input", "-i", default=None,
+                             choices=["acquire", "featurize", "train", "score", "evaluate"])
+    sb_pipeline.add_argument("--input", "-i", default=None, nargs="*",
                              help="Path to input data (optional, default = None)")
     sb_pipeline.add_argument("--config", default="config/config.yaml",
                              help="Path to configuration file")
-    sb_pipeline.add_argument("--output", "-o", default=None,
+    sb_pipeline.add_argument("--output", "-o", default=None, nargs="*",
                              help="Path to save output (optional, default = None)")
+    
+    # Sub-parser for plot feature importance
+    sb_plot = subparsers.add_parser("plot_feature_importance",
+                                        description="Generate bar plot for feature importance")
 
+    sb_plot.add_argument("--input", "-i", default=None,
+                             help="Path to input data (optional, default = None)")
+    sb_plot.add_argument("--config", default="config/config.yaml",
+                             help="Path to configuration file")
 
     args = parser.parse_args()
     sp_used = args.subparser_name
@@ -98,66 +85,176 @@ if __name__ == "__main__":
         upload_file_to_s3(args.local_path, args.s3_path)
     elif sp_used == "download_file_from_s3":
         download_file_from_s3(args.local_path, args.s3_path)
-    elif sp_used == 'ingest':
-        patient_manager = PatientManager(engine_string=args.engine_string)
-        patient_manager.add_patient(
-            args.id,
-            args.bmi,
-            args.smoking,
-            args.alcohol_drinking,
-            args.stroke,
-            args.physical_health,
-            args.mental_health,
-            args.diff_walking,
-            args.sex,
-            args.age_category,
-            args.race,
-            args.diabetic,
-            args.physical_activity,
-            args.gen_health,
-            args.sleep_time,
-            args.asthma,
-            args.kidney_disease,
-            args.skin_cancer
-        )
-        patient_manager.close()
     elif sp_used == 'run_model_pipeline':
         # load yaml configuration file
         try:
-            with open(args.config, "r") as f:
+            # Load configuration file for parameters and tmo path
+            with open(args.config, 'r') as f:
                 conf = yaml.load(f, Loader=yaml.FullLoader)
-                logger.info("Configuration file loaded from %s" % args.config)
+        # error when unable to find the config file
         except FileNotFoundError:
-            logger.error("Configuration file from %s is not found" % args.config)
+            logger.error('Configuration file from %s is not found', args.config)
+        else:
+            logger.info('Configuration file loaded from %s', args.config)
 
-        
+        # read input file to dataframe
         if args.input is not None:
-            input = pd.read_csv(args.input)
-            logger.info("Input data loaded from %s", args.input)
-        if args.step == "feature":
-            trans = get_binary_data(input, **conf["feature"]["get_binary_data"])
-            trans = get_ohe_data(trans, **conf["feature"]["get_ohe_data"])
-            trans = get_ordinalenc_age(trans, **conf["feature"]["get_ordinalenc_age"])
-            output = get_ordinalenc_health(trans, **conf["feature"]["get_ordinalenc_health"])
-            # output = normalization(trans)
+            if len(args.input) == 1:
+                if args.input[0].endswith('csv'):
+                    try:
+                        input = pd.read_csv(args.input[0])
+                    # error when unable to find the input file
+                    except FileNotFoundError:
+                        logger.error('No such file or directory, please check the input file.')
+                        sys.exit(1)
+                    except pd.errors.ParserError:
+                        logger.error('The input file has incorrect format')
+                        sys.exit(1)
+                    except pd.errors.EmptyDataError:
+                        logger.error('The data frame is empty')
+                        sys.exit(1)
+                    else:
+                        logger.info('Input data loaded from %s', args.input[0])
+            else:
+                input = []
+                for i in range(len(args.input)):
+                    # read csv
+                    if args.input[i].endswith('csv'):
+                        try:
+                            input.append(pd.read_csv(args.input[i]))
+                        # error when unable to find the input file
+                        except FileNotFoundError:
+                            logger.error('No such file or directory, please check the input file.')
+                            sys.exit(1)
+                        except pd.errors.ParserError:
+                            logger.error('The input file has incorrect format')
+                            sys.exit(1)
+                        except pd.errors.EmptyDataError:
+                            logger.error('The data frame is empty')
+                            sys.exit(1)
+                        else:
+                            logger.info('Input data loaded from %s', args.input[i])
+                    # load model
+                    elif args.input[i].endswith('sav'):
+                        try:
+                            with open(args.input[i], 'rb') as f:
+                                model = pickle.load(f)
+                        # common error
+                        except pickle.UnpicklingError as error_msg:
+                            logger.error(error_msg)
+                            sys.exit(1)
+                        # secondary errors
+                        except (AttributeError,  EOFError, ImportError, IndexError) as error_msg:
+                            logger.error(error_msg)
+                            sys.exit(1)
+                        # other errors
+                        except Exception as error_msg:
+                            logger.error(error_msg)
+                            sys.exit(1)
+                        else:
+                            input.append(model)
+        # model pipeline
+        if args.step == "acquire":
+            output = import_data(**conf["acquire"]["import_data"])
+        elif args.step == "featurize":
+            output = featurize(input, False, conf["feature"], **conf["feature"]["featurize"])
             logger.info('Feature engineering completed')
-        elif args.step == "model":
-            fitted = train(input,**conf['model']["train"])
-            output, x_test, y_test = fitted[0], fitted[1], fitted[2]
-            # evaluate the model result
-            evaluate(output, x_test, y_test, **conf['model']['evaluate'])
-        elif args.step == 'test':
-            os.system('pytest')
+        elif args.step == "train":
+            output = train(input,**conf['model']["train"])
+        elif args.step == "score":
+            output = score(input[0], input[1], **conf["model"]["score"])
+        elif args.step == "evaluate":
+            output = evaluate(input[0], input[1])
+        elif args.step == "feature_importance":
+            plot_feature_importance
 
         if args.output is not None:
-            if args.step != "model":
-                # save intermediate artifacts in the model pipeline
-                output.to_csv(args.output, index=False)
-                logger.info("Output saved to %s", args.output)
-            else:
+            if args.step == 'train':
                 # save the trained model
-                joblib.dump(output, args.output)
-                logger.info("Trained model object saved to %s", args.output)
+                try:
+                    with open(args.output[0] ,'wb') as f:
+                        pickle.dump(output[0], f)
+                except pickle.PicklingError as error_msg:
+                    logger.error(error_msg)
+                    sys.exit(1)
+                else:
+                    logger.info('Trained model object saved to %s', args.output[0])
+                
+                try:
+                    # save intermediate artifacts in the model pipeline
+
+                    output[1].to_csv(args.output[1], index=False)
+                    y_test = pd.DataFrame({"HeartDisease":output[2]})
+                    y_test.to_csv(args.output[2], index=False)
+                # error for incorrect output
+                except AttributeError as error_msg:
+                    logger.error(error_msg)
+                    sys.exit(1)
+                except TypeError as error_msg:
+                    logger.error(error_msg)
+                    sys.exit(1)
+                else:
+                    logger.info('Output saved to %s', args.output[1])
+                    logger.info('Output saved to %s', args.output[2])
+            elif args.step == 'evaluate':
+                try:
+                    # write all metrics to a file
+                    with open(args.output[0], 'w') as out_file:
+                        out_file.write(output[0])
+                        out_file.write(output[1])
+                        out_file.write(output[2])
+                        out_file.close()
+                # handle errors when unable to write file
+                except FileNotFoundError as error_msg:
+                    logger.error(error_msg)
+                    sys.exit(1)
+                else:
+                    logger.info('The evaluation results are recorded in %s', args.output[0])
+            else:
+                try:
+                    # save intermediate artifacts in the model pipeline
+                    output.to_csv(args.output[0], index=False)
+                # error for incorrect output
+                except AttributeError as error_msg:
+                    logger.error(error_msg)
+                    sys.exit(1)
+                except TypeError as error_msg:
+                    logger.error(error_msg)
+                    sys.exit(1)
+                else:
+                    logger.info('Output saved to %s', args.output[0])
+    elif sp_used == "plot_feature_importance":
+        # load yaml configuration file
+        try:
+            # Load configuration file for parameters and tmo path
+            with open(args.config, 'r') as f:
+                conf = yaml.load(f, Loader=yaml.FullLoader)
+        # error when unable to find the config file
+        except FileNotFoundError:
+            logger.error('Configuration file from %s is not found', args.config)
+        else:
+            logger.info('Configuration file loaded from %s', args.config)
+
+        # read input file to dataframe
+        if args.input is not None:
+            try:
+                with open(args.input, 'rb') as f:
+                    input = pickle.load(f)
+            # common error
+            except pickle.UnpicklingError as error_msg:
+                logger.error(error_msg)
+                sys.exit(1)
+            # secondary errors
+            except (AttributeError,  EOFError, ImportError, IndexError) as error_msg:
+                logger.error(error_msg)
+                sys.exit(1)
+            # other errors
+            except Exception as error_msg:
+                logger.error(error_msg)
+                sys.exit(1)
+            else:
+                logger.info("The model is loaded from %s", args.input)
+        plot_feature_importance(input, **conf["model"]["plot_feature_importance"])
     else:
         parser.print_help()
 
