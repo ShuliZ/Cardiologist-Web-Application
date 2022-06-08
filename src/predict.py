c1a69264918a9a9402ae10ca27d528bffc5314f8
Shuli Zhu <shulizhu2022@u.northwestern.edu>
2022-06-08 17:10:23 -0500

    Split functions

--- src/predict.py
+++ src/predict.py
@@ -1,22 +1,66 @@
-"""
-This module contains multiple functions that offer
-user input transformation and prediction functionality
-"""
 import logging
 import pickle
+import typing
 
 import pandas as pd
-import numpy as np
+import sklearn
 
 logger = logging.getLogger(__name__)
+# pylint: disable=W0703
 
+def input_predict(df: pd.DataFrame,
+                  rf_model: sklearn.base.BaseEstimator,
+                  transformed_col: typing.List[str]) -> typing.List[str]:
+    """return a list containing prediction labels and probabilities
+    based on user input variables
 
-def input_predict(df, model_path, transformed_col):
+    Args:
+        df (`pd.DataFrame`): data frame representation of the user input
+        rf_model (`sklearn.base.BaseEstimator`): the trained model used for prediction
+        transformed_col (`list` of `str`): column names used as features for prediction
+
+    Returns:
+        predict_result (`list` of `str`): a list of prediction result
+    """
+    predict_result = []
+    # predict the class with the new user input
+    try:
+        prediction = rf_model.predict(df[transformed_col])
+        pred_prob = round(100 * rf_model.predict_proba(df[transformed_col])[:, 1][0])
+    except KeyError as error_msg:
+        logger.warning('The column names are not matched.')
+        logger.error(error_msg)
+        return predict_result
+    except UnboundLocalError as error_msg:
+        logger.error(error_msg)
+        return predict_result
+    # get the prediction class and probabilities
+    else:
+        if prediction == 1:
+            pred_bin = 'You are LIKELY to have heart disease'
+        else:
+            pred_bin = 'You are NOT LIKELY to have heart disease'
+        predict_result = [pred_prob, pred_bin]
+        return predict_result
+
+def predict(df: pd.DataFrame,
+            config: typing.Dict,
+            model_path: str):
+    """perform prediction by loading the model from a specific path
+
+    Args:
+        df (`pd.DataFrame`): data frame representation of the user input variable
+        config (`dict`): configuration used for input_predict
+        model_path (`str`): the path to the trained model
+
+    Returns:
+        predict_result (`list` of `str`): a list of prediction result
+    """
     # load pre-trained model
     try:
-        with open(model_path, 'rb') as f:
+        with open(model_path, 'rb') as model_file:
             logger.info('Started import models.')
-            loaded_rf = pickle.load(f)
+            loaded_rf = pickle.load(model_file)
     # common error
     except pickle.UnpicklingError as error_msg:
         logger.error(error_msg)
@@ -28,22 +72,5 @@ def input_predict(df, model_path, transformed_col):
         logger.error(error_msg)
     else:
         logger.info('Loaded model from %s', model_path)
-
-    # predict the class with the new user input
-    try:
-        prediction = loaded_rf.predict(df[transformed_col])
-        pred_prob = round(100 * loaded_rf.predict_proba(df[transformed_col])[:, 1][0])
-        logger.debug("Pred prob is %s", pred_prob)
-    except KeyError as error_msg:
-        logger.warning('The column names are not matched.')
-        logger.error(error_msg)
-        return
-    except UnboundLocalError as error_msg:
-        logger.error(error_msg)
-        return
-    else:
-        if prediction == 1:
-            pred_bin = "You are LIKELY to have heart disease"
-        else:
-            pred_bin = "You are NOT LIKELY to have heart disease"
-        return [pred_prob, pred_bin]
+    predict_result = input_predict(df, loaded_rf, **config['input_predict'])
+    return predict_result
