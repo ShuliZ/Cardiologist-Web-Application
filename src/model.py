c7732ff5eec254b41c44a85837ab4cb0c4fe3631
Shuli Zhu <shulizhu2022@u.northwestern.edu>
2022-06-08 17:10:07 -0500

    Splitted functions

--- src/model.py
+++ src/model.py
@@ -1,120 +1,113 @@
-"""
-This module contains functions that perform
-model training and evaluation
-"""
 import logging
 import typing
+import pickle
 
 import pandas as pd
-import numpy as np
 import sklearn
-import matplotlib.pyplot as plt
-import seaborn as sns
 from imblearn.under_sampling import RandomUnderSampler
 
 logger = logging.getLogger(__name__)
 
-def train(trans_df, target, sampling_strat, rand_state, test_prop, n_estimate, min_split, min_leaf, max_depth):
-    X = trans_df.drop([target], axis=1)
-    y = trans_df[[target]].values.ravel()
 
+def train_test_split(df: pd.DataFrame,
+                     target: str,
+                     random_state: int,
+                     test_size: float = 0.2) -> typing.Tuple[pd.DataFrame, pd.DataFrame, pd.Series, pd.Series]:
+    """ Splits dataframe into train and test feature sets and targets.
+    Args:
+        df (`pd.DataFrame`): Data containing features and target
+        target_col (`str`): Column containing target values/labels
+        random_state (`int`): Random state to make split reproducible
+        test_size (`float`): Fraction of set to randomly sample to create test data
+    Returns:
+        x_train (`pd.DataFrame`): Features for training dataset
+        x_test (`pd.DataFrame`): Features for testing dataset
+        y_train (`pd.Series`): True target values for training dataset
+        y_test (`pd.Series`): True target values for testing dataset
+    """
+    # split features and target
+    features = df.drop([target], axis=1)
+    target = df[[target]].values.ravel()
     # train test split
-    x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X,y,test_size=test_prop,random_state=rand_state)
+    x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(
+        features, target, test_size=test_size, random_state=random_state)
 
-    # define oversampling strategy
-    undersample = RandomUnderSampler(sampling_strategy=sampling_strat, random_state=rand_state)
+    return x_train, x_test, y_train, y_test
+
+
+
+def fit_model(x_train: pd.DataFrame,
+              y_train: pd.Series,
+              model_params: dict,
+              sampling_strat: str,
+              rand_state: int,
+              model_type: str = 'RandomForestClassifier') -> sklearn.base.BaseEstimator:
+    """Fits a random forest classification model
+    Args:
+        x_train (`pd.DataFrame`): Features for training dataset
+        y_train (`pd.Series`): True target values for training dataset
+        model_params (`dict`): Dictionary containing keyword arguments for model instantiation.
+        sampling_strat (`str`): undersampling strategy
+        rand_state (`int`): seed for the undersampling model
+        model_type (`str`): Type of model to train. Default: 'RandomForestClassifier' Currently, this function is only
+            set up to train a `sklearn` random forest classifier but could easily be extended in future such that
+            multiple different model types could be trained with only a change in configuration.
+    Returns:
+        model (`sklearn.base.BaseEstimator`): Trained model object
+    """
+
+    # define undersampling strategy
+    undersample = RandomUnderSampler(
+        sampling_strategy=sampling_strat,random_state=rand_state)
 
     # fit and apply the transform
     x_under, y_under = undersample.fit_resample(x_train, y_train)
 
-    # random forest model
-    rf = sklearn.ensemble.RandomForestClassifier(n_estimators=n_estimate,
-                                                min_samples_split= min_split,
-                                                min_samples_leaf=min_leaf,
-                                                max_depth=max_depth,
-                                                random_state = rand_state)
-    rf.fit(x_under, y_under)
-    return [rf, x_test, y_test]
+    # fit random forest classifier
+    if model_type == 'RandomForestClassifier':
+        model = sklearn.ensemble.RandomForestClassifier(**model_params)
+    else:
+        logger.error('`fit_model` not configured for %s model type. Currently only '
+                     'configured for sklearn.ensemble.RandomForestClassifier', model_type)
+        raise ValueError(f'`fit_model` not configured for {model_type} model type. Currently only '
+                         'configured for sklearn.ensemble.RandomForestClassifier')
 
-def score(rf_model: sklearn.ensemble.RandomForestClassifier,
-          x_test: pd.DataFrame,
-          transformed_features: typing.List[str]) -> pd.DataFrame:
-    """a function used for model prediction and returns predicted scores and labels
-        Args:
-            rf_model (:obj:`sklearn.ensemble.RandomForestClassifier`)
-            x_test (:obj:`pandas.DataFrame`): a full series of features for testing
-            transformed_features (:obj:`list` of `str`): variables after feature engineering process
-        Return:
-            scored_df (:obj:`pandas.DataFrame`): a data frame that record the predicted results
+    model.fit(x_under, y_under)
+
+    logger.info('Trained model object created:\n%s', str(model))
+
+    return model
+
+
+def train_model(data: pd.DataFrame, config: dict, out_model: str, out_data: str) -> None:
+    """Orchestrate model training by creating train test split, training model,
+    and writing out trained model object
+    Args:
+        data (`pd.DataFrame`): feature engineered data frame for modeling
+        config (`dict`): configuration for splitting and training
+        out_model (`str`): output model path
+        out_data (`str`): output data path
+    Returns:
+        None
     """
-    # predict probability and class for each sample in the test set
-    scored_df = pd.DataFrame()
-    try:
-        ypred_proba_test = rf_model.predict_proba(x_test[transformed_features])[:,1]
-        ypred_bin_test = rf_model.predict(x_test[transformed_features])
-    # ValueError for incorrect initial_features
-    except ValueError as error_msg:
-        logger.error(error_msg)
-        return scored_df
-    # AttributeError for incorrect model input
-    except AttributeError as error_msg:
-        logger.error(error_msg)
-        return scored_df
+    # train test split
+    x_train, x_test, y_train, y_test = train_test_split(data, **config['train_test_split'])
+    # model fitting
+    model = fit_model(x_train, y_train, **config['fit_model'])
+    # write model
+    with open(out_model, 'wb') as file:
+        try:
+            pickle.dump(model, file)
+        except pickle.PickleError:
+            logger.error('Error while writing trained model object to %s', out_model)
+        else:
+            logger.info('Trained model object saved to %s', out_model)
+    # wirte training and tetsting data frame
+    target_col = config['train_test_split']['target']
+    x_train[target_col] = y_train
+    x_test[target_col] = y_test
+    x_train.to_csv(f'{out_data}train.csv', index=False)
+    logger.info('Training dataset saved to %s', f'{out_data}train.csv')
 
-    scored_df['ypred_proba_test'] = ypred_proba_test
-    scored_df['ypred_bin_test'] = ypred_bin_test
-    logger.info('Completed prediction')
-    return scored_df
-
-
-def evaluate(scored_df: pd.DataFrame,
-             y_test: pd.Series) -> None:
-    """ a function used to save all evaluation metrics
-        Args:
-            scored_df (:obj:`pandas.DataFrame`): a data frame that record the predicted results
-            y_test (:obj:`pandas.Series`): a series of target variable for testing
-            save_path (str): path to save the model
-        Return:
-            None
-    """
-    # predict probability and class for each sample in the test set
-    ypred_proba_test = scored_df['ypred_proba_test']
-    ypred_bin_test = scored_df['ypred_bin_test']
-    # evaluation metrics
-    auc = sklearn.metrics.roc_auc_score(y_test, ypred_proba_test)
-    confusion = sklearn.metrics.confusion_matrix(y_test, ypred_bin_test)
-    accuracy = sklearn.metrics.accuracy_score(y_test, ypred_bin_test)
-    tn, fp, fn, tp = confusion.ravel()
-    fnr = fn/(fn+tp)
-    logger.info('Completed evaluation of the Random Forest Classifier')
-
-    # convert confusion matrix value to dataframe
-    confusion_matrix = pd.DataFrame(confusion,
-                       index=['Actual negative', 'Actual positive'],
-                       columns=['Predicted negative', 'Predicted positive'])
-    # convert dataframe to string for writting to output files
-    df_string = confusion_matrix.to_string(header=True, index=True)
-    wtrite_acc = 'Accuracy on test: {:0.3f} \n'.format(accuracy)
-    write_fnr = 'False Negative Rate on test: {:0.3f} \n'.format(fnr)
-    return [wtrite_acc, write_fnr, df_string]
-
-def plot_feature_importance(rf_model, columns, save_path, top_n=15):
-
-    #Create arrays from feature importance and feature names
-    importance = rf_model.feature_importances_
-    feature_importance = np.array(importance)
-    feature_names = np.array(columns)
-
-    #Create a DataFrame using a Dictionary
-    data={'feature_names':feature_names,'feature_importance':feature_importance}
-    fi_df = pd.DataFrame(data)
-
-    #Sort the DataFrame in order decreasing feature importance
-    fi_df.sort_values(by=['feature_importance'], ascending=False,inplace=True)
-    fi_df = fi_df.iloc[:top_n,:]
-
-    #Plot Searborn bar chart
-    plt.figure(figsize=(10,8))
-    sns.barplot(x=fi_df['feature_importance'], y=fi_df['feature_names'])
-    plt.savefig(save_path)
-    logger.info("Completed generating the plot for feature importance.")
+    x_test.to_csv(f'{out_data}test.csv', index=False)
+    logger.info('Testing dataset saved to %s', f'{out_data}test.csv')
